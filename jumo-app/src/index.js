b8079e0a0dfaf1558efd5c574cd64846e877ddaf
ShinHeegook <hgsignal@gmail.com>
2021-04-08 14:42:28 +0900

    Save before ejecting

--- jumo-app/src/index.js
+++ jumo-app/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Detail from './pages/Detail';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Detail />
   </React.StrictMode>,
   document.getElementById('root'),
 );
