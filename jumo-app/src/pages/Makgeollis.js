702e3920fa591e94218cfb091cd52533c13fa6d1
ShinHeegook <hgsignal@gmail.com>
2021-04-11 11:12:49 +0900

    :lipstick: Add #24 - edit:Title
    탑 메뉴 title font size 수정

--- jumo-app/src/pages/Makgeollis.js
+++ jumo-app/src/pages/Makgeollis.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import makgeolli from '../images/mak-sample-1.png';
-import logo from '../images/logo.png';
-import ribbon from '../images/ribbon.png';
+// import logo from '../images/logo.png';
+// import ribbon from '../images/ribbon.png';
 
 const Makgeollis = () => {
   return (
     <StyleListTop>
       <StyleBestBox>
-        <div className="best-mak-title">많이 찾는 막걸리(TOP 10)</div>
+        <StyleTitle>많이 찾는 막걸리(TOP 10)</StyleTitle>
         <StyleBestMain>
           <StyleLeftArrow>{'<'}</StyleLeftArrow>
 
@@ -31,6 +31,14 @@ const Makgeollis = () => {
           <StyleRightArrow>{'>'}</StyleRightArrow>
         </StyleBestMain>
       </StyleBestBox>
+
+      <StyleMobileList>
+        <StyleMoboleImg />
+        <StyleBoder>
+          <StyleMoboleImg />
+        </StyleBoder>
+        <StyleMoboleImg />
+      </StyleMobileList>
     </StyleListTop>
   );
 };
@@ -38,6 +46,23 @@ const Makgeollis = () => {
 const StyleListTop = styled.div`
   width: 100vw;
   background-color: lightblue;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyleTitle = styled.div`
+  font-size: 2rem;
+  text-align: center;
+
+  @media ${props => props.theme.mobile} {
+  }
+
+  @media ${props => props.theme.tablet} {
+  }
+
+  @media ${props => props.theme.desktop} {
+    font-size: 1.5rem;
+  }
 `;
 
 const StyleBestBox = styled.div`
@@ -70,49 +95,49 @@ const StyleBestMain = styled.div`
   }
 `;
 
-const StyleMark = styled.div`
-  display: flex;
-`;
+// const StyleMark = styled.div`
+//   display: flex;
+// `;
 
-const StyleRank = styled.div`
-  width: 30px;
-  height: 25px;
-  background-color: lightskyblue;
-  color: white;
-  font-size: 1.8rem;
-  text-align: center;
-  font-weight: bold;
-  position: absolute;
-  left: 40px;
-  top: 80px;
-  z-index: 4;
-`;
+// const StyleRank = styled.div`
+//   width: 30px;
+//   height: 25px;
+//   background-color: lightskyblue;
+//   color: white;
+//   font-size: 1.8rem;
+//   text-align: center;
+//   font-weight: bold;
+//   position: absolute;
+//   left: 40px;
+//   top: 80px;
+//   z-index: 4;
+// `;
 
-const StyleLogo = styled.div`
-  background-image: url(${logo});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  width: 40px;
-  height: 40px;
-  position: absolute;
-  left: 10px;
-  top: 10px;
-  z-index: 3;
-`;
+// const StyleLogo = styled.div`
+//   background-image: url(${logo});
+//   background-size: cover;
+//   background-repeat: no-repeat;
+//   background-position: center;
+//   width: 40px;
+//   height: 40px;
+//   position: absolute;
+//   left: 10px;
+//   top: 10px;
+//   z-index: 3;
+// `;
 
-const StyleRibbon = styled.div`
-  background-image: url(${ribbon});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  width: 105px;
-  height: 100px;
-  position: absolute;
-  left: 15px;
-  top: 110px;
-  z-index: 2;
-`;
+// const StyleRibbon = styled.div`
+//   background-image: url(${ribbon});
+//   background-size: cover;
+//   background-repeat: no-repeat;
+//   background-position: center;
+//   width: 105px;
+//   height: 100px;
+//   position: absolute;
+//   left: 15px;
+//   top: 110px;
+//   z-index: 2;
+// `;
 
 const StyleLeftArrow = styled.div`
   @media ${props => props.theme.mobile} {
@@ -204,6 +229,36 @@ const StyleBoder = styled.div`
 
   @media ${props => props.theme.desktop} {
     /* border: none; */
+  }
+`;
+
+const StyleMobileList = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 5vmin 0;
+`;
+
+const StyleMoboleImg = styled.div`
+  display: block;
+  background-image: url(${makgeolli});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 100px;
+  height: 20vh;
+  margin: 2vmin;
+
+  @media ${props => props.theme.mobile} {
+  }
+
+  @media ${props => props.theme.tablet} {
+    display: none;
+  }
+
+  @media ${props => props.theme.desktop} {
+    width: 150px;
+    height: 30vh;
+    margin: 0 4vmin 0 0;
   }
 `;
 
