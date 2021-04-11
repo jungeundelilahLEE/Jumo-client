46a31d63ea959ce18d2f23f7d4a91f4c9be2e704
ShinHeegook <hgsignal@gmail.com>
2021-04-11 14:58:07 +0900

    :iphone: Add #24 - edit: Mobile View
    모바일, 테블릿 화면 UI 수정

--- jumo-app/src/pages/Makgeollis.js
+++ jumo-app/src/pages/Makgeollis.js
@@ -10,7 +10,7 @@ const Makgeollis = () => {
         개의 막걸리가 검색되었습니다.
       </StyleResultText>
       <StyleListBox>
-        <div className="StyleCard">
+        <StyleCard>
           <StyleItem>
             <StyleItemInfo>
               <StyleInfoTop>
@@ -26,14 +26,168 @@ const Makgeollis = () => {
               </StyleInfoBox>
             </StyleItemInfo>
           </StyleItem>
-        </div>
+          <StyleMobileInfoBox>
+            <div>느린마을 막걸리입니다아아아아</div>
+            <div>
+              <div className="StyleMobileViews">Views: 1002</div>
+              <div className="StyleMobileLikes">Likes: 500</div>
+              <div className="StyleMobileReviews">Reviews: 7809</div>
+            </div>
+          </StyleMobileInfoBox>
+        </StyleCard>
 
-        <StyleItem />
-        <StyleItem />
-        <StyleItem />
-        <StyleItem />
-        <StyleItem />
-        <StyleItem />
+        {/* TEST UI */}
+        <StyleCard>
+          <StyleItem>
+            <StyleItemInfo>
+              <StyleInfoTop>
+                <div className="StyleInfoName">
+                  느린마을 막걸리입니다아아아아
+                </div>
+                <StyleInfoVol>4.5 % vol</StyleInfoVol>
+              </StyleInfoTop>
+              <StyleInfoBox>
+                <div className="StyleViews">Views: 1002</div>
+                <div className="StyleLikes">Likes: 500</div>
+                <div className="StyleReviews">Reviews: 7809</div>
+              </StyleInfoBox>
+            </StyleItemInfo>
+          </StyleItem>
+          <StyleMobileInfoBox>
+            <div>느린마을 막걸리입니다아아아아</div>
+            <div>
+              <div className="StyleMobileViews">Views: 1002</div>
+              <div className="StyleMobileLikes">Likes: 500</div>
+              <div className="StyleMobileReviews">Reviews: 7809</div>
+            </div>
+          </StyleMobileInfoBox>
+        </StyleCard>
+        <StyleCard>
+          <StyleItem>
+            <StyleItemInfo>
+              <StyleInfoTop>
+                <div className="StyleInfoName">
+                  느린마을 막걸리입니다아아아아
+                </div>
+                <StyleInfoVol>4.5 % vol</StyleInfoVol>
+              </StyleInfoTop>
+              <StyleInfoBox>
+                <div className="StyleViews">Views: 1002</div>
+                <div className="StyleLikes">Likes: 500</div>
+                <div className="StyleReviews">Reviews: 7809</div>
+              </StyleInfoBox>
+            </StyleItemInfo>
+          </StyleItem>
+          <StyleMobileInfoBox>
+            <div>느린마을 막걸리입니다아아아아</div>
+            <div>
+              <div className="StyleMobileViews">Views: 1002</div>
+              <div className="StyleMobileLikes">Likes: 500</div>
+              <div className="StyleMobileReviews">Reviews: 7809</div>
+            </div>
+          </StyleMobileInfoBox>
+        </StyleCard>
+        <StyleCard>
+          <StyleItem>
+            <StyleItemInfo>
+              <StyleInfoTop>
+                <div className="StyleInfoName">
+                  느린마을 막걸리입니다아아아아
+                </div>
+                <StyleInfoVol>4.5 % vol</StyleInfoVol>
+              </StyleInfoTop>
+              <StyleInfoBox>
+                <div className="StyleViews">Views: 1002</div>
+                <div className="StyleLikes">Likes: 500</div>
+                <div className="StyleReviews">Reviews: 7809</div>
+              </StyleInfoBox>
+            </StyleItemInfo>
+          </StyleItem>
+          <StyleMobileInfoBox>
+            <div>느린마을 막걸리입니다아아아아</div>
+            <div>
+              <div className="StyleMobileViews">Views: 1002</div>
+              <div className="StyleMobileLikes">Likes: 500</div>
+              <div className="StyleMobileReviews">Reviews: 7809</div>
+            </div>
+          </StyleMobileInfoBox>
+        </StyleCard>
+        <StyleCard>
+          <StyleItem>
+            <StyleItemInfo>
+              <StyleInfoTop>
+                <div className="StyleInfoName">
+                  느린마을 막걸리입니다아아아아
+                </div>
+                <StyleInfoVol>4.5 % vol</StyleInfoVol>
+              </StyleInfoTop>
+              <StyleInfoBox>
+                <div className="StyleViews">Views: 1002</div>
+                <div className="StyleLikes">Likes: 500</div>
+                <div className="StyleReviews">Reviews: 7809</div>
+              </StyleInfoBox>
+            </StyleItemInfo>
+          </StyleItem>
+          <StyleMobileInfoBox>
+            <div>느린마을 막걸리입니다아아아아</div>
+            <div>
+              <div className="StyleMobileViews">Views: 1002</div>
+              <div className="StyleMobileLikes">Likes: 500</div>
+              <div className="StyleMobileReviews">Reviews: 7809</div>
+            </div>
+          </StyleMobileInfoBox>
+        </StyleCard>
+        <StyleCard>
+          <StyleItem>
+            <StyleItemInfo>
+              <StyleInfoTop>
+                <div className="StyleInfoName">
+                  느린마을 막걸리입니다아아아아
+                </div>
+                <StyleInfoVol>4.5 % vol</StyleInfoVol>
+              </StyleInfoTop>
+              <StyleInfoBox>
+                <div className="StyleViews">Views: 1002</div>
+                <div className="StyleLikes">Likes: 500</div>
+                <div className="StyleReviews">Reviews: 7809</div>
+              </StyleInfoBox>
+            </StyleItemInfo>
+          </StyleItem>
+          <StyleMobileInfoBox>
+            <div>느린마을 막걸리입니다아아아아</div>
+            <div>
+              <div className="StyleMobileViews">Views: 1002</div>
+              <div className="StyleMobileLikes">Likes: 500</div>
+              <div className="StyleMobileReviews">Reviews: 7809</div>
+            </div>
+          </StyleMobileInfoBox>
+        </StyleCard>
+        <StyleCard>
+          <StyleItem>
+            <StyleItemInfo>
+              <StyleInfoTop>
+                <div className="StyleInfoName">
+                  느린마을 막걸리입니다아아아아
+                </div>
+                <StyleInfoVol>4.5 % vol</StyleInfoVol>
+              </StyleInfoTop>
+              <StyleInfoBox>
+                <div className="StyleViews">Views: 1002</div>
+                <div className="StyleLikes">Likes: 500</div>
+                <div className="StyleReviews">Reviews: 7809</div>
+              </StyleInfoBox>
+            </StyleItemInfo>
+          </StyleItem>
+          <StyleMobileInfoBox>
+            <div>느린마을 막걸리입니다아아아아</div>
+            <div>
+              <div className="StyleMobileViews">Views: 1002</div>
+              <div className="StyleMobileLikes">Likes: 500</div>
+              <div className="StyleMobileReviews">Reviews: 7809</div>
+            </div>
+          </StyleMobileInfoBox>
+        </StyleCard>
+        {/* TEST UI */}
       </StyleListBox>
     </StyleMakgeollis>
   );
@@ -85,23 +239,47 @@ const StyleListBox = styled.div`
   flex-wrap: wrap;
   justify-content: space-evenly;
   padding: 0;
+  width: 95vw;
+  margin: 0 auto;
 
   @media ${props => props.theme.mobile} {
   }
 
   @media ${props => props.theme.tablet} {
-    width: 80vw;
-    /* width: 800px; */
-    margin: 0 auto;
-    justify-content: start;
-  }
-
-  @media screen and (min-width: 800px) {
+    /* width: 80vw; */
     width: 800px;
+    justify-content: start;
   }
 
   @media ${props => props.theme.desktop} {
     width: 870px;
+  }
+`;
+
+const StyleCard = styled.div`
+  background-color: #ffffff;
+  background-color: rgba(255, 255, 255, 0.5);
+  padding: 0;
+  width: 170px;
+  border-radius: 1vmin;
+  margin-bottom: 4vmin;
+
+  @media ${props => props.theme.mobile} {
+  }
+
+  @media ${props => props.theme.tablet} {
+    margin: 2vmin 2vmin;
+  }
+
+  @media screen and (min-width: 1100px) {
+    margin: 2vmin 1vmin;
+  }
+
+  @media ${props => props.theme.desktop} {
+    background-color: transparent !important;
+    margin: 3vmin 20px;
+    width: 130px;
+    height: 30vh;
   }
 `;
 
@@ -151,32 +329,44 @@ const StyleItem = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-  width: 170px;
+  width: 100%;
   height: 35vh;
-  margin: 3vmin 1vmin;
-
-  &:hover ${StyleItemInfo} {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-  }
+  margin: 3vmin 0;
 
   @media ${props => props.theme.mobile} {
   }
 
   @media ${props => props.theme.tablet} {
-    margin: 2vmin 2vmin;
+    margin: 2vmin 0;
   }
 
   @media screen and (min-width: 1100px) {
-    margin: 2vmin 1vmin;
+    &:hover ${StyleItemInfo} {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-evenly;
+    }
   }
 
   @media ${props => props.theme.desktop} {
-    margin: 3vmin 20px;
+    margin: 3vmin 0;
     width: 130px;
     height: 30vh;
+  }
+`;
+
+const StyleMobileInfoBox = styled.div`
+  padding: 1.5vmin;
+
+  @media ${props => props.theme.mobile} {
+  }
+
+  @media ${props => props.theme.tablet} {
+  }
+
+  @media ${props => props.theme.desktop} {
+    display: none;
   }
 `;
 
