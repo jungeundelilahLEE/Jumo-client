8cc0876ddaf30dc664a4632e5175075651a8a5aa
ShinHeegook <hgsignal@gmail.com>
2021-04-11 11:20:23 +0900

    :truck: #24 - edit:file name

--- jumo-app/src/index.js
+++ jumo-app/src/index.js
@@ -4,13 +4,13 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyles from './styles/GlobalStyles';
 // import Detail from './pages/Detail';
-import Makgeollis from './pages/Makgeollis';
+import BestMakgeollis from './pages/BestMakgeollis';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Makgeollis />
+      <BestMakgeollis />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
