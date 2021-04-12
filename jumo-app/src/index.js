862b2e229bd6d49e1e225efe4664516e2ea64841
ShinHeegook <hgsignal@gmail.com>
2021-04-12 13:46:11 +0900

    :construction: upstream pull

--- jumo-app/src/index.js
+++ jumo-app/src/index.js
@@ -4,13 +4,12 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyles from './styles/GlobalStyles';
 // import Detail from './pages/Detail';
-import BestMakgeollis from './pages/BestMakgeollis';
+// import BestMakgeollis from './pages/BestMakgeollis';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <BestMakgeollis />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
