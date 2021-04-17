1523af09f9ebd70d019e8ed5dcd917b605bc09ca
ShinHeegook <hgsignal@gmail.com>
2021-04-17 15:39:55 +0900

    :sparkles: Add #77 - edit: brewery list
    더미데이터 테스트 완료

--- jumo-app/src/atoms/dummyBrewery.js
+++ jumo-app/src/atoms/dummyBrewery.js
@@ -6,6 +6,9 @@ const res = {
       address: '강원도 철원군 김화읍 청양리 2889',
       number: 334587676,
       kinds: '초가우리쌀막걸리',
+      makgeolli_id: 18,
+      map:
+        'https://s3.ap-northeast-2.amazonaws.com/jumo.ml/NewPhoto/%E1%84%8E%E1%85%A9%E1%84%80%E1%85%A1%E1%84%8B%E1%85%AE%E1%84%85%E1%85%B5%E1%84%8A%E1%85%A1%E1%86%AF%E1%84%86%E1%85%A1%E1%86%A8%E1%84%80%E1%85%A5%E1%86%AF%E1%84%85%E1%85%B5.jpg',
       createdAt: null,
       updatedAt: null,
     },
@@ -15,6 +18,9 @@ const res = {
       address: '경기 포천시 일동면 성장로 411',
       number: 315356833,
       kinds: '포천일동막걸리',
+      makgeolli_id: 19,
+      map:
+        'https://s3.ap-northeast-2.amazonaws.com/jumo.ml/NewPhoto/%E1%84%80%E1%85%A1%E1%84%91%E1%85%A7%E1%86%BC%E1%84%8C%E1%85%A1%E1%86%BA%E1%84%86%E1%85%A1%E1%86%A8%E1%84%80%E1%85%A5%E1%86%AF%E1%84%85%E1%85%B5.jpg',
       createdAt: null,
       updatedAt: null,
     },
