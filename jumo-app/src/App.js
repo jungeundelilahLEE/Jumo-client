d1ebb80542950a27b522798b299e14d8b5f45867
ShinHeegook <hgsignal@gmail.com>
2021-04-24 13:47:24 +0900

    :sparkles: Add #149 - edit: Login
    로그인/로그아웃 routing, enter 키 이벤트 설정

--- jumo-app/src/App.js
+++ jumo-app/src/App.js
@@ -109,11 +109,15 @@ const App = () => {
         open={openSignIn}
         closeHandler={closeHandler}
         signupModalHandler={signupModalHandler}
+        navHeader={navHeader}
+        setNavHeader={setNavHeader}
       />
       <SignUp
         close={openSignUp}
         openHandler={openHandler}
         closeHandler={closeHandler}
+        navHeader={navHeader}
+        setNavHeader={setNavHeader}
       />
       <Switch>
         <Route exact path="/">
